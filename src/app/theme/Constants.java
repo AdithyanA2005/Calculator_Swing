b5a6fd1d53de4c1bafa79fb2e10bad8f3ced4201
Adithyan A <deepaadithyan56@gmail.com>
2022-01-21 17:47:34 +0530

    Variables like colors and fonts are added to constant file

---
+++ src/app/theme/Constants.java
@@ -0,0 +1,14 @@
+package app.theme;
+import java.awt.Font;
+import java.awt.Color;
+
+public class Constants {
+    public static Font main_font = new Font("Helvetica", Font.PLAIN, 40);
+    public static Font title_font = new Font("Helvetica", Font.PLAIN, 50);
+    
+    public static Color rgb_red = new Color(249, 62, 62);
+    public static Color rgb_grey = new Color(223, 235, 242);
+    public static Color rgb_blue = new Color(31, 41, 51);
+    public static Color rgb_white = new Color(245, 247, 250);
+    public static Color rgb_orange = new Color (249, 112, 62);    
+}
